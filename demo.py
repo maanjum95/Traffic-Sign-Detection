7f90fc9229157300428dbec275ca38065739a4af
Muhammad Anjum <ma.anjum95@gmail.com>
2022-01-26 12:10:05 +0100

    Parameter and fps display.

--- demo.py
+++ demo.py
@@ -9,6 +9,7 @@ if os.uname()[1] == "raspberrypi":
     from re_terminal import setup_btns
 else:
     import model
+    setup_btns = None
 
 import signs
 
@@ -20,17 +21,30 @@ def setup_cv_window():
 
     return window_name
 
+def show_param_fps(img, model_params, fps):
+    img_h, img_w, _ = img.shape
+
+    # show parameters
+    cv.putText(img, str(model_params), (0, img_h - 6), cv.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
+
+    # fps
+    cv.putText(img, f"Fps: {round(fps, 2)}", (img_w - 60, 10), cv.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
+
 def webcam_demo():
     window = setup_cv_window()
     model_dict = model.setup_model()
     model_params = model.default_params()
     sign_imgs = signs.load_imgs()
 
+    if setup_btns is not None:
+        setup_btns(model_params)
+
     # webcam
     webcam = cv.VideoCapture(0)
 
     start_time = time.time()
     frame_count = 0
+    fps = 0.0
 
     while True:
         _, img = webcam.read()
@@ -41,8 +55,15 @@ def webcam_demo():
         img, predictions = model.run_algorithm_on_img(img, model_dict, model_params)
 
         # add signs imags
-        signs.add_signs_to_img(predictions)
-        signs.show_signs(img, sign_imgs, img_w, img_h)
+        if model_params["show_signs"]:
+            signs.add_signs_to_img(predictions, model_params["sign_threshold"])
+            signs.show_signs(img, sign_imgs, img_w, img_h)
+        else:
+            # remove signs to show
+            signs.signs_to_show = {}
+
+        # show parameters and fps
+        show_param_fps(img, model_params, fps)
 
         elapsed_time = time.time() - start_time
         fps = frame_count / elapsed_time
@@ -58,14 +79,17 @@ def video_demo(video_path):
     window = setup_cv_window()
     model_dict = model.setup_model()
     model_params = model.default_params()
-    setup_btns(model_params)
     sign_imgs = signs.load_imgs() 
+
+    if setup_btns is not None:
+        setup_btns(model_params)
 
     # video
     video = cv.VideoCapture(video_path)
 
     exe_start = time.time()
     frame_count = 0
+    fps = 0.0
 
     while True:
         _, img = video.read()
@@ -83,8 +107,8 @@ def video_demo(video_path):
             # remove signs to show
             signs.signs_to_show = {}
 
-        # show parameters
-        cv.putText(img, str(model_params), (0, img_h - 6), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        # show parameters and fps
+        show_param_fps(img, model_params, fps)
 
         #if len(predictions) > 0:
         #    cv.imwrite(f"output/{frame_count}.jpg", img)
